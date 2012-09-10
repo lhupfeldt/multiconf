a0420f84b22ba960c27260645b95c8398c115a44
Aleksey Maksimov <ctpeko3a@gmail.com>
2012-09-10 18:04:26 +0200

    Added additional comments in examples

--- demo/valid_envs.py
+++ demo/valid_envs.py
@@ -3,12 +3,16 @@
 
 from multiconf.envs import Env, EnvGroup
 
-preprod = Env('preprod')
-prod = Env('prod')
-g_prod = EnvGroup('g_prod', preprod, prod)
-
+# Define environments
+#
+# Just instantiate object Env with environment name
+#
 devLocal = Env('devLocal')
-
 devi = Env('devi')
 devs = Env('devs')
+preprod = Env('preprod')
+prod = Env('prod')
+
+# Define group of environments
+g_prod = EnvGroup('g_prod', preprod, prod)
 g_dev = EnvGroup('g_dev', devi, devs)
