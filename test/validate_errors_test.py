1d6131ebdfec2a8bce1dcc19b32ae2385a4a25b4
Lars Hupfeldt Nielsen <lhn@ole2.(none)>
2013-03-31 18:21:22 +0200

    Added tests for failing user validate methods

---
+++ test/validate_errors_test.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+# Copyright (c) 2012 Lars Hupfeldt Nielsen, Hupfeldt IT
+# All rights reserved. This work is under a BSD license, see LICENSE.TXT.
+
+# pylint: disable=E0611
+from pytest import raises
+
+from .. import ConfigRoot, ConfigItem, ConfigBuilder
+from ..envs import EnvFactory
+
+
+ef = EnvFactory()
+prod = ef.Env('prod')
+
+
+def test_root_validate_error():
+    class root(ConfigRoot):
+        def validate(self):
+            raise Exception("Error in root validate")
+
+    with raises(Exception) as exinfo:
+        with root(prod, [prod]):
+            pass
+
+    assert exinfo.value.message == "Error in root validate"
+
+
+def test_item_validate_error():
+    class item(ConfigItem):
+        def validate(self):
+            raise Exception("Error in item validate")
+
+    with raises(Exception) as exinfo:
+        with ConfigRoot(prod, [prod]):
+            item()
+
+    assert exinfo.value.message == "Error in item validate"
+
+
+def test_builder_validate_error():
+    class builder(ConfigBuilder):
+        def build(self):
+            pass
+
+        def validate(self):
+            raise Exception("Error in builder validate")
+
+    with raises(Exception) as exinfo:
+        with ConfigRoot(prod, [prod]):
+            builder()
+
+    assert exinfo.value.message == "Error in builder validate"
