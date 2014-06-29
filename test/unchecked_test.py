98b623113d8e2de897b4e2bd3109b9c461163de3
Lars Hupfeldt <lhn@hupfeldtit.dk>
2014-06-29 23:58:32 +0200

    Minor test improvement and cleanup

--- test/unchecked_test.py
+++ test/unchecked_test.py
@@ -16,9 +16,9 @@ ef = EnvFactory()
 def ce(line_num, *lines):
     return config_error(__file__, line_num, *lines)
 
-dev2ct = ef.Env('dev2ct')
-dev2st = ef.Env('dev2st')
-g_dev = ef.EnvGroup('g_dev', dev2ct, dev2st)
+dev1 = ef.Env('dev1')
+dev2 = ef.Env('dev2')
+g_dev = ef.EnvGroup('g_dev', dev1, dev2)
 
 pp = ef.Env('pp')
 prod = ef.Env('prod')
@@ -54,6 +54,8 @@ def test_required_missing_unchecked_for_configroot():
         cr.setattr('anotherattr', prod=2)
 
     assert cr.anattr == 1
+    with raises(Exception) as exinfo:
+        cr.anotherattr
 
 
 def test_required_missing_unchecked_for_configitem():
@@ -65,7 +67,7 @@ def test_required_missing_unchecked_for_configitem():
     assert cr.uitem.anattr == 2
     assert cr.uitem.anotherattr == 2
 
-    with ConfigRoot(dev2ct, [g_dev, pp, prod]) as cr:
+    with ConfigRoot(dev1, [g_dev, pp, prod]) as cr:
         with uitem() as it:
             it.setattr('anattr', pp=1, g_dev=1)
             it.setattr('anotherattr', prod=2)
@@ -82,7 +84,7 @@ def test_required_missing_unchecked_base_for_configitem():
     assert cr.item.anattr == 2
     assert cr.item.anotherattr == 2
 
-    with ConfigRoot(dev2ct, [g_dev, pp, prod]) as cr:
+    with ConfigRoot(dev1, [g_dev, pp, prod]) as cr:
         with item() as it:
             it.setattr('anattr', pp=1, g_dev=1)
             it.setattr('anotherattr', prod=2, pp=1, g_dev=0)
@@ -91,14 +93,14 @@ def test_required_missing_unchecked_base_for_configitem():
     assert cr.item.anotherattr == 0
 
 
-_required_missing_unchecked_super_for_configitem_expected1a = """Attribute: 'anotherattr' did not receive a value for env Env('dev2ct'), which is a member of EnvGroup('g_dev') {
-     Env('dev2ct'),
-     Env('dev2st')
+_required_missing_unchecked_super_for_configitem_expected1a = """Attribute: 'anotherattr' did not receive a value for env Env('dev1'), which is a member of EnvGroup('g_dev') {
+     Env('dev1'),
+     Env('dev2')
 }"""
 
-_required_missing_unchecked_super_for_configitem_expected1b = """Attribute: 'anotherattr' did not receive a value for env Env('dev2st'), which is a member of EnvGroup('g_dev') {
-     Env('dev2ct'),
-     Env('dev2st')
+_required_missing_unchecked_super_for_configitem_expected1b = """Attribute: 'anotherattr' did not receive a value for env Env('dev2'), which is a member of EnvGroup('g_dev') {
+     Env('dev1'),
+     Env('dev2')
 }"""
 
 _required_missing_unchecked_super_for_configitem_expected1_ex = """There were 2 errors when defining attribute 'anotherattr' on object: {
@@ -135,7 +137,7 @@ def test_required_missing_unchecked_super_for_configitem(capsys):
     assert replace_ids(exinfo.value.message, False) == _required_missing_unchecked_super_for_configitem_expected1_ex
 
     with raises(ConfigException) as exinfo:
-        with ConfigRoot(dev2ct, [g_dev, pp, prod]):
+        with ConfigRoot(dev1, [g_dev, pp, prod]):
             with item() as it:
                 it.setattr('anotherattr', prod=2, pp=1, g_dev=0)
                 errorline = lineno()
@@ -145,7 +147,7 @@ def test_required_missing_unchecked_super_for_configitem(capsys):
     assert replace_ids(exinfo.value.message, False) == _required_missing_unchecked_super_for_configitem_expected2_ex
 
     with raises(ConfigException) as exinfo:
-        with ConfigRoot(dev2ct, [g_dev, pp, prod]):
+        with ConfigRoot(dev1, [g_dev, pp, prod]):
             with item() as it:
                 it.setattr('anattr', g_dev=1)
                 it.setattr('anotherattr', prod=2, pp=1, g_dev=0)
