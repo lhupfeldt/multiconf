b34fe7062be61312c4b4e8881fee61b863b15b98
Lars Hupfeldt <lhn@hupfeldtit.dk>
2015-02-15 12:50:36 +0100

    pylint

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2012 Lars Hupfeldt Nielsen, Hupfeldt IT
 # All rights reserved. This work is under a BSD license, see LICENSE.TXT.
 
-# pylint: disable=W0611
+# pylint: disable=unused-import
 from .multiconf import ConfigRoot, ConfigBuilder, ConfigItem
 from .config_errors import caller_file_line, ConfigException, ConfigDefinitionException, ConfigApiException, InvalidUsageException
 from .values import MC_REQUIRED, MC_TODO
