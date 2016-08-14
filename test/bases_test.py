25a6f5693ce2524c36396bcc3065ca050c75a0d8
Lars Hupfeldt <lhn@hupfeldtit.dk>
2016-08-14 22:53:27 +0200

    Improved test coverage

---
+++ test/bases_test.py
@@ -0,0 +1,10 @@
+from ..bases import get_bases
+
+
+def test_bases():
+    assert list(get_bases(object)) == [object]
+
+    class Xx(object):
+        pass
+
+    assert list(get_bases(Xx)) == [Xx, object]
