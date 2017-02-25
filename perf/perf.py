543ada61e8e0f57cbd1ef16ef7fb9288697b93c5
Lars Hupfeldt <lhn@hupfeldtit.dk>
2017-02-25 23:11:42 +0100

    Updated perf test

--- perf/perf.py
+++ perf/perf.py
@@ -91,42 +91,42 @@ def perf1():
 
         for ii in range(0, second_range):
             with rchild_default(name=repr(ii), aa=4) as ci:
-                ci.setattr('a' + repr(ii), default=2)
+                ci.setattr('a' + repr(ii) + '?', default=2)
 
         for ii in range(0, third_range):
             with rchild_env(name=repr(ii)) as ci:
-                ci.setattr('aa' + repr(ii), dev1=7, dev2=8, tst=9, pp=18, prod=5)
-                ci.setattr('bb' + repr(ii), g_dev=8, tst=9, pp=19, prod=3)
-                ci.setattr('x' + repr(ii), g_dev=8, tst=9, pp=19, prod=3)
-                ci.setattr('y' + repr(ii), g_dev=8, tst=9, pp=19, prod=3)
-                ci.setattr('z' + repr(ii), g_dev=8, tst=9, pp=19, prod=3)
+                ci.setattr('aa' + repr(ii) + '?', dev1=7, dev2=8, tst=9, pp=18, prod=5)
+                ci.setattr('bb' + repr(ii) + '?', g_dev=8, tst=9, pp=19, prod=3)
+                ci.setattr('x' + repr(ii) + '?', g_dev=8, tst=9, pp=19, prod=3)
+                ci.setattr('y' + repr(ii) + '?', g_dev=8, tst=9, pp=19, prod=3)
+                ci.setattr('z' + repr(ii) + '?', g_dev=8, tst=9, pp=19, prod=3)
 
                 with rchild_env(name='a' + repr(ii)) as ci:
-                    ci.setattr('cc' + repr(ii), dev1=7, dev2=8, tst=9, pp=18, prod=5)
-                    ci.setattr('dd' + repr(ii), g_dev=8, tst=9, pp=19, prod=3)
+                    ci.setattr('cc' + repr(ii) + '?', dev1=7, dev2=8, tst=9, pp=18, prod=5)
+                    ci.setattr('dd' + repr(ii) + '?', g_dev=8, tst=9, pp=19, prod=3)
 
                     for jj in range(0, 10):
-                        with rchild_default(name='ee' + repr(ii) + '_' + repr(jj), aa=4) as ci:
-                            ci.setattr('ff' + repr(ii) + '_' + repr(jj), default=2, pp=3, prod=4)
+                        with rchild_default(name='ee' + repr(ii) + '_' + repr(jj) + '?', aa=4) as ci:
+                            ci.setattr('ff' + repr(ii) + '_' + repr(jj) + '?', default=2, pp=3, prod=4)
 
-                        with rchild_mc_init(name='gg' + repr(ii) + '_' + repr(jj), xx=7) as ci:
-                            ci.setattr('hh' + repr(ii) + '_' + repr(jj), default=2, pp=3, prod=4)
-                            ci.setattr('ii' + repr(ii) + '_' + repr(jj), default=2, pp=3, prod=4)
+                        with rchild_mc_init(name='gg' + repr(ii) + '_' + repr(jj) + '?', xx=7) as ci:
+                            ci.setattr('hh' + repr(ii) + '_' + repr(jj) + '?', default=2, pp=3, prod=4)
+                            ci.setattr('ii' + repr(ii) + '_' + repr(jj) + '?', default=2, pp=3, prod=4)
 
                     with rchild_default(name='jj' + repr(ii), aa="hello there") as ci:
-                        ci.setattr('kk' + repr(ii), default="hello there!", pp="hello somewhere", prod="what?")
+                        ci.setattr('kk' + repr(ii) + '?', default="hello there!", pp="hello somewhere", prod="what?")
 
                     with rchild_default(name='c' + repr(ii), aa="HELLO THERE") as ci:
-                        ci.setattr('ll' + repr(ii), default="HELLO THERE!", pp="HELLO SOMEWHERE", prod="WHAT?")
+                        ci.setattr('ll' + repr(ii) + '?', default="HELLO THERE!", pp="HELLO SOMEWHERE", prod="WHAT?")
                         ci.setattr('aa', pp="HELLO SOMEWHERE", prod="WHAT?")
 
                 with rchild_env(name='nn' + repr(ii)) as ci:
-                    ci.setattr('oo' + repr(ii), dev1=7, dev2=8, tst=9, pp=18, prod=5)
-                    ci.setattr('pp' + repr(ii), g_dev=8, tst=9, pp=19, prod=3)
+                    ci.setattr('oo' + repr(ii) + '?', dev1=7, dev2=8, tst=9, pp=18, prod=5)
+                    ci.setattr('pp' + repr(ii) + '?', g_dev=8, tst=9, pp=19, prod=3)
 
                 with rchild_env(name='c' + repr(ii)) as ci:
-                    ci.setattr('qq' + repr(ii), default=7, g_dev=8, tst=9, pp=18, prod=5)
-                    ci.setattr('rr' + repr(ii), g_dev=8, tst=9, pp=19, prod=3)
+                    ci.setattr('qq' + repr(ii) + '?', default=7, g_dev=8, tst=9, pp=18, prod=5)
+                    ci.setattr('rr' + repr(ii) + '?', g_dev=8, tst=9, pp=19, prod=3)
 
     if heap_check:
         print(hp.heap())
