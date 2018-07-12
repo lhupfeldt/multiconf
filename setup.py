b58e53c6e64ffd8e4078955bb0986be387557a6e
Lars Hupfeldt <lhn@hupfeldtit.dk>
2018-07-12 08:40:28 +0200

    Added 3.7 to classifiers

--- setup.py
+++ setup.py
@@ -66,6 +66,7 @@ if __name__ == "__main__":
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
             'Topic :: System :: Installation/Setup',
             'Topic :: Software Development :: Testing',
         ],
