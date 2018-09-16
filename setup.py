ce97e2e2a180235e8602f021c1c85ba67d4655b1
Lars Hupfeldt <lhn@hupfeldtit.dk>
2018-09-16 15:05:40 +0200

    Official Python 3.8 support - 3.8 not allowed in setup.py yet

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ if __name__ == "__main__":
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
+            # 'Programming Language :: Python :: 3.8',
             'Topic :: System :: Installation/Setup',
             'Topic :: Software Development :: Testing',
         ],
