c4ff8fd4c592a8ec2ebc92d2749bb3f0f6bf40be
Lars Hupfeldt <lhn@hupfeldtit.dk>
2019-11-24 17:01:23 +0100

    Test: multiple configs different envs

--- test/multiple_configs_test.py
+++ test/multiple_configs_test.py
@@ -41,3 +41,43 @@ def test_multiple_configs_same_env_factory():
     cr1 = config1(pp)
     assert cr1.ItemWithAABB.aa == 1
     assert cr2.ItemWithAABB.aa == 3
+
+
+def test_multiple_configs_different_env_factory():
+    # Check that getting a config for another env does not change other configs current env
+
+    ef2 = EnvFactory()
+    ef2.Env('dev1')
+    pp2 = ef2.Env('pp')
+    prod2 = ef2.Env('prod')
+
+    def aa():
+        @mc_config(ef, load_now=True)
+        def config(_):
+            with ItemWithAABB() as ci:
+                ci.setattr('aa', default=1, prod=2)
+        return config
+
+    def bb():
+        @mc_config(ef2)
+        def config(_):
+            with ItemWithAABB() as ci:
+                ci.setattr('aa', default=1, prod=3)
+        return config
+
+    cr1 = aa()(prod)
+    assert cr1.ItemWithAABB.aa == 2
+
+    cfg = bb()
+    cfg.load()
+    cr2 = cfg(pp2)
+    assert cr2.ItemWithAABB.aa == 1
+    assert cr1.ItemWithAABB.aa == 2
+
+    cr2 = cfg(prod2)
+    assert cr2.ItemWithAABB.aa == 3
+    assert cr1.ItemWithAABB.aa == 2
+
+    cr1 = aa()(pp)
+    assert cr1.ItemWithAABB.aa == 1
+    assert cr2.ItemWithAABB.aa == 3
