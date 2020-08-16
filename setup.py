7b4901d72a0096b79360f6d501985b8812e9166d
Lars Hupfeldt <lhn@hupfeldtit.dk>
2020-08-16 12:55:05 +0200

    Add Python 3.8 to setup.py as supported - it already was

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
             'Operating System :: OS Independent',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
-            # 'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.8',
             'Topic :: System :: Installation/Setup',
             'Topic :: Software Development :: Testing',
         ],
